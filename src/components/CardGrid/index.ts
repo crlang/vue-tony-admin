7cdda703417c34e23a6ec5c404780c4256cfef58
crlang <crlang@163.com>
2022-01-11 10:12:13 +0800

    fix(CardGrid): before initialization

--- src/components/CardGrid/index.ts
+++ src/components/CardGrid/index.ts
@@ -3,7 +3,7 @@ import cardGrid from './src/index.vue'
 import cardGridItem from './src/item.vue'
 
 export const CardGrid = withInstall(cardGrid, {
-  CardGridItem,
+  cardGridItem,
 })
 export default CardGrid
 export const CardGridItem = withNoopInstall(cardGridItem)
