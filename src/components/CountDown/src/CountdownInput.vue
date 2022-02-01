84a91728068f1f9b4a659ba0d5b6e129e853207a
crlang <crlang@163.com>
2022-02-01 16:19:24 +0800

    fix(CountDown): adjust related parameters

--- src/components/CountDown/src/CountdownInput.vue
+++ src/components/CountDown/src/CountdownInput.vue
@@ -2,21 +2,12 @@
   <ElInput
     v-bind="$attrs"
     :class="prefixCls"
-    :size="size"
-    v-model:modelValue="state">
+    v-model="state">
     <template #append>
       <CountButton
-        :size="size"
         :count="count"
         :value="state"
         :beforeStartFunc="sendCodeApi" />
-    </template>
-    <template
-      #[item]="data"
-      v-for="item in Object.keys($slots).filter((k) => k !== 'append')">
-      <slot
-        :name="item"
-        v-bind="data || {}"></slot>
     </template>
   </ElInput>
 </template>
@@ -29,8 +20,6 @@ import { useDesign } from '@/hooks/web/useDesign'
 import { useRuleFormItem } from '@/hooks/component/useFormItem'
 
 const props = {
-  modelValue: { type: String },
-  size: { type: String, validator: (v:string) => ['large', 'default', 'small', ''].includes(v) },
   count: { type: Number, default: 60 },
   sendCodeApi: {
     type: Function as PropType<() => Promise<boolean>>,
@@ -51,10 +40,3 @@ export default defineComponent({
   },
 })
 </script>
-<style lang="scss">
-$prefix-cls: '#{$tonyname}-countdown-input';
-
-.#{$prefix-cls} {
-  position: relative;
-}
-</style>
