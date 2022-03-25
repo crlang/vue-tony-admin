f56031ff1b137e132c13cbaaf33c6b1c882914d4
crlang <crlang@163.com>
2022-03-25 10:59:49 +0800

    revert: withdraw demo data

--- src/layouts/default/header/components/notify/data.ts
+++ src/layouts/default/header/components/notify/data.ts
@@ -3,7 +3,9 @@ import { EleTag } from '@/components/ElementPlus'
 export interface ListItem {
   id: string
   avatar: string
+  // 通知的标题内容
   title: string
+  // 是否在标题上显示删除线
   titleDelete?: boolean
   datetime: string
   type: string
@@ -24,12 +26,12 @@ export interface TabItem {
 export const tabListData: TabItem[] = [
   {
     key: '1',
-    name: 'Notify',
+    name: '通知',
     list: [
       {
         id: '000000001',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png',
-        title: '',
+        title: '你收到了 14 份新周报',
         description: '',
         datetime: '2021-08-09',
         type: '1',
@@ -37,7 +39,7 @@ export const tabListData: TabItem[] = [
       {
         id: '000000002',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png',
-        title: '',
+        title: '你推荐的 曲妮妮 已通过第三轮面试',
         description: '',
         datetime: '2021-08-08',
         type: '1',
@@ -45,7 +47,7 @@ export const tabListData: TabItem[] = [
       {
         id: '000000003',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/kISTdvpyTAhtGxpovNWd.png',
-        title: '',
+        title: '这种模板可以区分多种通知类型',
         description: '',
         datetime: '2021-08-07',
         // read: true,
@@ -54,7 +56,7 @@ export const tabListData: TabItem[] = [
       {
         id: '000000004',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png',
-        title: '',
+        title: '左侧图标用于区分不同的类型',
         description: '',
         datetime: '2021-08-07',
         type: '1',
@@ -63,7 +65,7 @@ export const tabListData: TabItem[] = [
         id: '000000005',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png',
         title:
-          '',
+          '标题可以设置自动显示省略号，本例中标题行数已设为1行，如果内容超过1行将自动截断并支持tooltip显示完整标题。',
         description: '',
         datetime: '2021-08-07',
         type: '1',
@@ -71,7 +73,7 @@ export const tabListData: TabItem[] = [
       {
         id: '000000006',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png',
-        title: '',
+        title: '左侧图标用于区分不同的类型',
         description: '',
         datetime: '2021-08-07',
         type: '1',
@@ -79,7 +81,7 @@ export const tabListData: TabItem[] = [
       {
         id: '000000007',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png',
-        title: '',
+        title: '左侧图标用于区分不同的类型',
         description: '',
         datetime: '2021-08-07',
         type: '1',
@@ -87,7 +89,7 @@ export const tabListData: TabItem[] = [
       {
         id: '000000008',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png',
-        title: '',
+        title: '左侧图标用于区分不同的类型',
         description: '',
         datetime: '2021-08-07',
         type: '1',
@@ -95,7 +97,7 @@ export const tabListData: TabItem[] = [
       {
         id: '000000009',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png',
-        title: '',
+        title: '左侧图标用于区分不同的类型',
         description: '',
         datetime: '2021-08-07',
         type: '1',
@@ -103,7 +105,7 @@ export const tabListData: TabItem[] = [
       {
         id: '000000010',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png',
-        title: '',
+        title: '左侧图标用于区分不同的类型',
         description: '',
         datetime: '2021-08-07',
         type: '1',
@@ -112,13 +114,13 @@ export const tabListData: TabItem[] = [
   },
   {
     key: '2',
-    name: 'Message',
+    name: '消息',
     list: [
       {
         id: '000000006',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
-        title: '',
-        description: '',
+        title: '曲丽丽 评论了你',
+        description: '描述信息描述信息描述信息',
         datetime: '2021-08-07',
         type: '2',
         clickClose: true,
@@ -126,8 +128,8 @@ export const tabListData: TabItem[] = [
       {
         id: '000000007',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
-        title: '',
-        description: '',
+        title: '朱偏右 回复了你',
+        description: '这种模板用于提醒谁与你发生了互动',
         datetime: '2021-08-07',
         type: '2',
         clickClose: true,
@@ -135,9 +137,9 @@ export const tabListData: TabItem[] = [
       {
         id: '000000008',
         avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
-        title: '',
+        title: '标题',
         description:
-          '',
+          '请将鼠标移动到此处，以便测试超长的消息在此处将如何处理。本例中设置的描述最大行数为2，超过2行的描述内容将被省略并且可以通过tooltip查看完整内容',
         datetime: '2021-08-07',
         type: '2',
         clickClose: true,
@@ -146,45 +148,45 @@ export const tabListData: TabItem[] = [
   },
   {
     key: '3',
-    name: 'Plan',
+    name: '待办',
     list: [
       {
         id: '000000009',
         avatar: '',
-        title: '',
-        description: ' 2021-01-12 20:00 ',
+        title: '任务名称',
+        description: '任务需要在 2021-01-12 20:00 前启动',
         datetime: '',
-        extra: '',
+        extra: '未开始',
         color: 'info',
         type: '3',
       },
       {
         id: '000000010',
         avatar: '',
-        title: '',
-        description: '  2021-01-07 ',
+        title: '第三方紧急代码变更',
+        description: '冠霖 需在 2021-01-07 前完成代码变更任务',
         datetime: '',
-        extra: '',
+        extra: '马上到期',
         color: 'warning',
         type: '3',
       },
       {
         id: '000000011',
         avatar: '',
-        title: '',
-        description: ' 2021-01-09 ',
+        title: '信息安全考试',
+        description: '指派竹尔于 2021-01-09 前完成更新并发布',
         datetime: '',
-        extra: ' 8 ',
+        extra: '已耗时 8 天',
         color: 'success',
         type: '3',
       },
       {
         id: '000000012',
         avatar: '',
-        title: 'ABCD ',
-        description: ' 2021-01-09 ',
+        title: 'ABCD 版本发布',
+        description: '指派竹尔于 2021-01-09 前完成更新并发布',
         datetime: '',
-        extra: '',
+        extra: '进行中',
         color: '',
         type: '3',
       },
