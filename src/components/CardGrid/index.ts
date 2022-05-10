646cdfd259a95e2304a2b5f554f2d759dc03f85e
crlang <crlang@163.com>
2022-05-10 16:04:55 +0800

    workflow: uniform naming

--- src/components/CardGrid/index.ts
+++ src/components/CardGrid/index.ts
@@ -1,6 +1,6 @@
 import { withInstall } from '@/utils'
-import cardGrid from './src/index.vue'
-import cardGridItem from './src/item.vue'
+import cardGrid from './src/CardGrid.vue'
+import cardGridItem from './src/CardGridItem.vue'
 
 export const CardGrid = withInstall(cardGrid)
 export const CardGridItem = withInstall(cardGridItem)
