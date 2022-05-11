465c172de80dc63bd5a3c700950d4dd588f9f921
crlang <crlang@163.com>
2022-05-11 17:51:16 +0800

    fix: change the name to avoid conflicts

--- src/components/Transition/index.ts
+++ src/components/Transition/index.ts
@@ -1,34 +1,34 @@
 import { createSimpleTransition, createJavascriptTransition, createExpandFunc } from './src/Transition'
 
 // Fade
-export const FadeTransition = createSimpleTransition('fade')
-export const FadeSlideTransition = createSimpleTransition('fade-slide')
-export const FadeBottomTransition = createSimpleTransition('fade-bottom')
-export const FadeScaleTransition = createSimpleTransition('fade-scale')
-export const FadeTopTransition = createSimpleTransition('fade-top')
+export const FadeTransition = createSimpleTransition('tyani-fade')
+export const FadeSlideTransition = createSimpleTransition('tyani-fade-slide')
+export const FadeBottomTransition = createSimpleTransition('tyani-fade-bottom')
+export const FadeScaleTransition = createSimpleTransition('tyani-fade-scale')
+export const FadeTopTransition = createSimpleTransition('tyani-fade-top')
 
 // Scale
-export const ScaleTransition = createSimpleTransition('scale')
-export const ScaleRotateTransition = createSimpleTransition('scale-rotate')
+export const ScaleTransition = createSimpleTransition('tyani-scale')
+export const ScaleRotateTransition = createSimpleTransition('tyani-scale-rotate')
 
 // Slide
-export const SlideTransition = createSimpleTransition('slide')
-export const SlideXTransition = createSimpleTransition('slide-x')
-export const SlideReverseTransition = createSimpleTransition('slide-reverse')
-export const SlideXReverseTransition = createSimpleTransition('slide-x-reverse')
+export const SlideTransition = createSimpleTransition('tyani-slide')
+export const SlideXTransition = createSimpleTransition('tyani-slide-x')
+export const SlideReverseTransition = createSimpleTransition('tyani-slide-reverse')
+export const SlideXReverseTransition = createSimpleTransition('tyani-slide-x-reverse')
 
 // Scroll
-export const ScrollTransition = createSimpleTransition('scroll')
-export const ScrollXTransition = createSimpleTransition('scroll-x')
-export const ScrollReverseTransition = createSimpleTransition('scroll-reverse')
-export const ScrollXReverseTransition = createSimpleTransition('scroll-x-reverse')
+export const ScrollTransition = createSimpleTransition('tyani-scroll')
+export const ScrollXTransition = createSimpleTransition('tyani-scroll-x')
+export const ScrollReverseTransition = createSimpleTransition('tyani-scroll-reverse')
+export const ScrollXReverseTransition = createSimpleTransition('tyani-scroll-x-reverse')
 
 // Zoom
-export const ZoomFadeTransition = createSimpleTransition('zoom-fade')
-export const ZoomOutTransition = createSimpleTransition('zoom-out')
+export const ZoomFadeTransition = createSimpleTransition('tyani-zoom-fade')
+export const ZoomOutTransition = createSimpleTransition('tyani-zoom-out')
 
 // JS 方式创建
 
 // Expand
-export const ExpandTransition = createJavascriptTransition('expand', createExpandFunc())
-export const ExpandXTransition = createJavascriptTransition('expand-x', createExpandFunc(true))
+export const ExpandTransition = createJavascriptTransition('tyani-expand', createExpandFunc())
+export const ExpandXTransition = createJavascriptTransition('tyani-expand-x', createExpandFunc(true))
