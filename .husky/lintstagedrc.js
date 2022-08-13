f16942516728eedfe77dbb99ee4cd6bf2bf157fd
crlang <crlang@163.com>
2022-08-13 22:27:49 +0800

    wip: commit rule change

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix'],
-  '{!(package)*.json,.!(browserslist)*rc}': ['prettier --write--parser json'],
-  'package.json': ['prettier --write'],
   '*.vue': ['eslint --fix', 'stylelint --fix'],
   '*.{scss,less,styl,html}': ['stylelint --fix'],
-  '*.md': ['prettier --write']
+  '*.{md,json}': ['prettier --write']
 }
